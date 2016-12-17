973942c2fbf5be77117979b5b344bc6dd81206aa
vinayak <vinupatil9@gmail.com>
2016-12-17 18:05:24 +0530

    grid on daskboard change vgright pc

--- MyApplication/app/src/main/java/com/example/vin/myapplication/Login.java
+++ MyApplication/app/src/main/java/com/example/vin/myapplication/Login.java
@@ -1,7 +1,10 @@
 package com.example.vin.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+
+        Button btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent k = new Intent(Login.this,DashBoard.class);
+                startActivity(k);
+            }
+        });
     }
 }
